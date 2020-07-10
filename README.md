3dcff05d37ffd44c25adc1beed7bc2fcfbf7bfe1
doit-now <namnguyenke1999@gmail.com>
2020-07-10 15:17:37 +0700

    1st: first project upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# Wel come to Nam Repository
+This repository contains a simple Java application userd to demonstrate 
+the CI - Continuous Integration process, a part of Introduction to
+Software Engineering course at FPT University, HCMC Campus
+
+## You will find in this repo:
+* MathUtil class: holds all of out own Math methods
+* Test class: demonstrate on how to ensure the software quality. 
+The tecnics applied: TDD, Unit Testing (JUnit)
+* .gitignore
+* Markdown file: this file itself
+
+## Connect me via:
+1. [GitHub](https://github.com/kenam1999)
+2. [Facebook](https://facebook.com/kenam1999)
+
+#### 2020 by Nam
---
+++ src/nam/util/MathUtil.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nam.util;
+
+/**
+ *
+ * @author Namng
+ */
+//đây là class chứa các hàm toán học ta tự viêt, mục đích là công cụ dùng chung
+//Cái ào xày chung cho mọi nơi thì dùng kĩ thuật static
+public class MathUtil {
+
+    //tính n!= 1 . 2 . 3. 4....n n! tăng rát nhanh vì n! sẽ tràng int rất nhanh, nên dùng long
+    //tính tối đa đến 15!  
+    public static long computeFactorial(int n) {
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+    
+
+}
